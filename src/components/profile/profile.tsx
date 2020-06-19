c45069edaae9afa8a4f96a4374128565ad31ce7f
Andre Marques <utilizador@MBP-de-Andre.Home>
2020-06-19 10:56:51 +0100

    Small cleaning in project

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './profile.scss';
+
+export const Profile = () => {
+    return (
+        <div className="header">
+            <img src="@images/netflix.png" alt="Netflix"></img>
+        </div>
+    )
+}
