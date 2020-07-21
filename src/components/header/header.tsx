a430c1b016e2397fd4e64dadd8a7b8b6ae91df4f
André Marques <ctw00530@criticaltechworks.com>
2020-07-21 16:02:03 +0100

    Added BEM methodology to header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,69 +15,69 @@ export const Header = () => {
     const username = useSelector((state: RootState) => state.user.username);
     return (
         <div className="header">
-            <div className="left-header">
+            <div className="header__left-menu">
                 <img src={netflixLogo}
-                    className="netflix-logo"
+                    className="header__left-menu-logo"
                     alt="Netflix"></img>
-                <ul hidden={username.length === 0} className="left-list">
-                    <li className="categories-text label-active">Home</li>
-                    <li className="categories-text">TV Shows</li>
-                    <li className="categories-text">Movies</li>
-                    <li className="categories-text">Recently Added</li>
-                    <li className="categories-text">My List</li>
+                <ul hidden={username.length === 0} className="header__left-menu-list">
+                    <li className="header__left-menu-list_text header__left-menu-list_active">Home</li>
+                    <li className="header__left-menu-list_text">TV Shows</li>
+                    <li className="header__left-menu-list_text">Movies</li>
+                    <li className="header__left-menu-list_text">Recently Added</li>
+                    <li className="header__left-menu-list_text">My List</li>
                 </ul>
-                <div hidden={username.length === 0} className="left-dropdown">
+                <div hidden={username.length === 0} className="header__left-menu-dropdown">
                     Search
                     <img src={arrowUp}
-                        className="arrow-down-icon"
+                        className="header__left-menu-dropdown_icon"
                         alt="Refer"></img>
                 </div>
             </div>
 
-            <div hidden={username.length === 0} className="right-header">
+            <div hidden={username.length === 0} className="header__right-menu">
                 <Search />
                 <img src={giftWhite}
-                    className="gift-icon"
+                    className="header__right-menu_gift-icon"
                     alt="Refer"></img>
-                <div className='notification-container'>
+                <div className='header__right-menu__notification-container'>
                     <img src={bellWhite}
-                        className="bell-icon"
+                        className="header__right-menu_bell-icon"
                         alt="Notification">
                     </img>
-                    <div className="notification-area">
-                        <div className="arrow-up-container">
+                    <div className="header__right-menu__notification-container__notification-area">
+                        <div className="header__right-menu__notification-container__notification-area__arrow-up">
                             <img src={arrowUp}
-                                className="arrow-up-icon"
+                                className="header__right-menu__notification-container__notification-area__arrow-up_icon"
                                 alt="Refer"></img>
                         </div>
-                        <div className="notification-zone">
+                        <div className="header__right-menu__notification-container__notification-area__list">
                             No recent notifications
                         </div>
                     </div>
                 </div>
-                <div className="avatar-container">
-                    <img className="avatar-icon" src={avatar1} alt="Avatar" />
+                <div className="header__right-menu__avatar-container">
+                    <img className="header__right-menu__avatar-container_icon" src={avatar1} alt="Avatar" />
                     <img src={arrowUp}
-                        className="arrow-down-icon"
+                        className="header__right-menu__avatar-container_arrow-down"
                         alt="Refer"></img>
-                    <div className="avatar-options">
+                    <div className="header__right-menu__avatar-container__options">
                         <img src={arrowUp}
-                            className="arrow-up-avatar"
+                            className="header__right-menu__avatar-container__options_arrow-up"
                             alt="Refer"></img>
-                        <div className="list-options">
-                            <div className="profile-option">
-                                <img className="avatar-icon" src={avatar1} alt="Avatar" />
-                                <span className="profile-text">James Peterson</span>
+                        <div className="header__right-menu__avatar-container__options__list-options">
+                            <div className="header__right-menu__avatar-container__options__list-options__row">
+                                <img className="header__right-menu__avatar-container__options__list-options__row_icon" src={avatar1} alt="Avatar" />
+                                <span className="header__right-menu__avatar-container__options__list-options__row_text">James Peterson</span>
                             </div>
-                            <div className="profile-option">
-                                <img className="avatar-icon" src={avatar2} alt="Avatar" />
-                                <span className="profile-text">Holland Maggie</span>
+                            <div className="header__right-menu__avatar-container__options__list-options__row">
+                                <img className="header__right-menu__avatar-container__options__list-options__row_icon" src={avatar2} alt="Avatar" />
+                                <span className="header__right-menu__avatar-container__options__list-options__row_text">Holland Maggie</span>
                             </div>
-                            <span className="manage-profiles">Manage profiles</span>
-                            <div className="separator"></div>
-                            <span className="settings-option">Account</span>
-                            <span className="settings-option">Help Center</span>
-                            <span className="settings-option">Logout</span>
+                            <span className="header__right-menu__avatar-container__options__list-options_manage-profiles">Manage profiles</span>
+                            <div className="header__right-menu__avatar-container__options__list-options_separator"></div>
+                            <span className="header__right-menu__avatar-container__options__list-options_settings-option">Account</span>
+                            <span className="header__right-menu__avatar-container__options__list-options_settings-option">Help Center</span>
+                            <span className="header__right-menu__avatar-container__options__list-options_settings-option">Logout</span>
                         </div>
                     </div>
                 </div>
@@ -89,8 +89,8 @@ export const Header = () => {
 function Search() {
     const ref = useRef() as React.MutableRefObject<HTMLInputElement>;;
     const [searchActive, setSearchActive] = useState(false);
-    const [searchBarClass, setSearchBarClass] = useState('search-bar');
-    const [searchInputClass, setSearchInputClass] = useState('search-input');
+    const [searchBarClass, setSearchBarClass] = useState('header__right-menu_search-bar');
+    const [searchInputClass, setSearchInputClass] = useState('header__right-menu_search-input');
 
     onOutsideClick(ref, () => {
         if (searchActive) {
@@ -101,12 +101,12 @@ function Search() {
 
     const setClassBasedOnSearch = () => {
         if (!searchActive) {
-            setSearchBarClass('search-bar active');
-            setSearchInputClass('search-input active')
+            setSearchBarClass('header__right-menu_search-bar header__right-menu_search-bar_active');
+            setSearchInputClass('header__right-menu_search-input header__right-menu_search-input_active')
         } else {
-            setSearchInputClass('search-input');
+            setSearchInputClass('header__right-menu_search-input');
             setTimeout(() => {
-                setSearchBarClass('search-bar');
+                setSearchBarClass('header__right-menu_search-bar');
             }, 500);
         }
     }
@@ -118,7 +118,7 @@ function Search() {
                 setClassBasedOnSearch();
                 setSearchActive(!searchActive);
             }} src={searchWhite}
-                className="search-icon"
+                className="header__right-menu_search-icon"
                 alt="Search"></img>
             <input ref={ref} className={searchInputClass} placeholder="Titles, people, genres" />
         </div>
